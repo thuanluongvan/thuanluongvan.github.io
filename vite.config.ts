e2a72ad7ebd3cd3d3d0a69f3387fe256280b74fd
Thuan <thuanluongvan.dev@gmail.com>
2023-07-16 18:05:49 +0700

    Fix router

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  return config
 })
